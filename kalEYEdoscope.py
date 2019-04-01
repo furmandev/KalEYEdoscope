24acc25d0ef32d29dd647571a094b25073bbb4cb
Andrew Furman <andrew@efurman.com>
2019-04-01 15:31:27 -0400

    argument handling for keyboard input, command-line input, and hdmi display

--- kalEYEdoscope.py
+++ kalEYEdoscope.py
@@ -9,11 +9,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import yaml
 from pylab import sin
-import RPi.GPIO as GPIO
-import OLED_Driver as OLED
-import Image
-import ImageDraw
-import ImageFont
 
 # DEFINE CONSTANT VARIABLES
 
@@ -23,9 +18,27 @@ HEIGHT = 128
 BUTTON1 = 14
 BUTTON2 = 15
 INTYPE = "BUTTON"
+DISPLAYTYPE = "OLED"
+
+# Interpret command-line arguments
 if len(sys.argv) > 1:
-    if sys.argv[1] == "-k":
+    if "-k" in sys.argv:
         INTYPE = "KEYBOARD"
+    if "-d" in sys.argv:
+        DISPLAYTYPE = "HDMI"
+        WIDTH = 384
+        HEIGHT = 384
+        import pygame
+        import os
+        import operator
+    else:
+        import RPi.GPIO as GPIO
+        import OLED_Driver as OLED
+        import Image
+        import ImageDraw
+        import ImageFont
+    if "-c" in sys.argv:
+        INTYPE = "SHELL"
 
 
 # DEFINE CONSTANT FUNCTIONS
@@ -43,26 +56,43 @@ def get_input():
                 return 1
             elif GPIO.input(BUTTON2) == GPIO.HIGH:
                 return 2
-    elif INTYPE == "KEYBOARD":
+    elif INTYPE == "SHELL":
         while True:
-            i = raw_input("input: ")
+            if sys.version_info[0] < 3:
+                i = raw_input("input: ")
+            else:
+                i = input("input: ")
             if i == "12": program_quit()
             if i == "1": return 1
             if i == "2": return 2
+    elif INTYPE == "KEYBOARD":
+        while True:
+            for e in pygame.event.get():
+                if e.type == pygame.KEYDOWN:
+                    if e.key == pygame.K_UP: return 1
+                    if e.key == pygame.K_DOWN: return 2
+                    if e.key == pygame.K_q: program_quit()
 
 
 def clear_screen():
     """
     Clears the screen by filling with background
     """
-    OLED.Clear_Screen()
-    clear_image = Image.new("RGB", (OLED.SSD1351_WIDTH, OLED.SSD1351_HEIGHT), "DIMGREY")
-    clear_draw = ImageDraw.Draw(clear_image)
-    return clear_draw, clear_image
+    if DISPLAYTYPE == "OLED":
+        OLED.Clear_Screen()
+        clear_image = Image.new("RGB", (OLED.SSD1351_WIDTH, OLED.SSD1351_HEIGHT), "DIMGREY")
+        clear_draw = ImageDraw.Draw(clear_image)
+        return clear_draw, clear_image
+    elif DISPLAYTYPE == "HDMI":
+        screen.fill(color_background)
+        return None, None
 
 
-def update(update_image):
-    OLED.Display_Image(update_image)
+def update(update_image=None):
+    if DISPLAYTYPE == "OLED":
+        OLED.Display_Image(update_image)
+    elif DISPLAYTYPE == "HDMI":
+        pygame.display.flip()
 
 
 def update_data():
@@ -73,7 +103,7 @@ def update_data():
         yaml.dump(data, save_data)
 
 
-def update_buttons(update_draw, update_image, left, right, title=None, subtitle=None):
+def update_buttons(left, right, title=None, subtitle=None, update_draw=None, update_image=None):
     """
     Updates the text displayed for each button instruction
     :param update_image:
@@ -83,56 +113,116 @@ def update_buttons(update_draw, update_image, left, right, title=None, subtitle=
     :param left: text to be displayed for the left button
     :param right: text to be displayed for the right button
     """
+    if DISPLAYTYPE == "OLED":
+        right_text = update_draw.textsize(right, font=font_normal)
+        left_text = update_draw.textsize(left, font=font_normal)
 
-    right_text = update_draw.textsize(right, font=font_normal)
-    left_text = update_draw.textsize(left, font=font_normal)
-
-    update_draw.rectangle([(WIDTH // 2 - right_text[0] // 2 - padding, HEIGHT // 2),
-                           (WIDTH // 2 + right_text[0] // 2 + padding, HEIGHT // 2 + right_text[1] + padding)],
-                          fill="BLUE")
-
-    update_draw.rectangle([(WIDTH // 2 - left_text[0] // 2 - padding, HEIGHT * 3 // 4),
-                           (WIDTH // 2 + left_text[0] // 2 + padding, HEIGHT * 3 // 4 + left_text[1] + padding)],
-                          fill="DARKORANGE")
-
-    update_draw.text((WIDTH // 2 - right_text[0] // 2, HEIGHT // 2), right, font=font_normal)
-    update_draw.text((WIDTH // 2 - left_text[0] // 2, HEIGHT * 3 // 4), left, font=font_normal)
-
-    if title:
-        title_text = update_draw.textsize(title, font_subtitle)
-        update_draw.text((WIDTH // 2 - title_text[0] // 2, padding), title, font=font_subtitle)
-
-    if subtitle:
-        subtitle_text = update_draw.textsize(subtitle, font_subtitle)
-        update_draw.text((WIDTH // 2 - subtitle_text[0] // 2, subtitle_text[1] + padding * 2), subtitle,
-                         font=font_subtitle)
-
-    update(update_image)
-    button = get_input()
-    if button == 1:
         update_draw.rectangle([(WIDTH // 2 - right_text[0] // 2 - padding, HEIGHT // 2),
                                (WIDTH // 2 + right_text[0] // 2 + padding, HEIGHT // 2 + right_text[1] + padding)],
-                              outline="BLACK")
-        update(update_image)
-        return 1
-    if button == 2:
+                              fill="BLUE")
+
         update_draw.rectangle([(WIDTH // 2 - left_text[0] // 2 - padding, HEIGHT * 3 // 4),
                                (WIDTH // 2 + left_text[0] // 2 + padding, HEIGHT * 3 // 4 + left_text[1] + padding)],
-                              outline="BLACK")
+                              fill="DARKORANGE")
+
+        update_draw.text((WIDTH // 2 - right_text[0] // 2, HEIGHT // 2), right, font=font_normal)
+        update_draw.text((WIDTH // 2 - left_text[0] // 2, HEIGHT * 3 // 4), left, font=font_normal)
+
+        if title:
+            title_text = update_draw.textsize(title, font_subtitle)
+            update_draw.text((WIDTH // 2 - title_text[0] // 2, padding), title, font=font_subtitle)
+
+        if subtitle:
+            subtitle_text = update_draw.textsize(subtitle, font_subtitle)
+            update_draw.text((WIDTH // 2 - subtitle_text[0] // 2, subtitle_text[1] + padding * 2), subtitle,
+                             font=font_subtitle)
+
         update(update_image)
-        return 2
+        button = get_input()
+        if button == 1:
+            update_draw.rectangle([(WIDTH // 2 - right_text[0] // 2 - padding, HEIGHT // 2),
+                                   (WIDTH // 2 + right_text[0] // 2 + padding, HEIGHT // 2 + right_text[1] + padding)],
+                                  outline="BLACK")
+            update(update_image)
+            return 1
+        if button == 2:
+            update_draw.rectangle([(WIDTH // 2 - left_text[0] // 2 - padding, HEIGHT * 3 // 4),
+                                   (
+                                       WIDTH // 2 + left_text[0] // 2 + padding,
+                                       HEIGHT * 3 // 4 + left_text[1] + padding)],
+                                  outline="BLACK")
+            update(update_image)
+            return 2
+
+    elif DISPLAYTYPE == "HDMI":
+        right_text = font_normal.render(right, True, color_white)
+        left_text = font_normal.render(left, True, color_white)
+        right_surface = pygame.Surface(tuple(map(operator.add, font_normal.size(right), (5, 5))))
+        left_surface = pygame.Surface(tuple(map(operator.add, font_normal.size(left), (7, 7))))
+        right_surface.fill(color_background)
+        left_surface.fill(color_background)
+
+        pygame.draw.rect(right_surface, color_right, right_surface.get_rect())
+        pygame.draw.rect(left_surface, color_left, left_surface.get_rect())
+
+        right_surface.blit(right_text, (right_surface.get_width() // 2 - right_text.get_width() // 2,
+                                        right_surface.get_height() // 2 - right_text.get_height() // 2))
+        left_surface.blit(left_text, (left_surface.get_width() // 2 - left_text.get_width() // 2,
+                                      left_surface.get_height() // 2 - left_text.get_height() // 2))
+
+        screen.blit(right_surface, (WIDTH // 2 - right_surface.get_width() // 2, HEIGHT // 2))
+        screen.blit(left_surface, (WIDTH // 2 - left_surface.get_width() // 2, HEIGHT * 3 // 4))
+
+        if title:
+            title_text = font_title.render(title, True, color_white)
+            screen.blit(title_text, (WIDTH // 2 - title_text.get_width() // 2, title_text.get_height()))
+
+        if subtitle:
+            subtitle_text = font_title.render(subtitle, True, color_white)
+            screen.blit(subtitle_text, (WIDTH // 2 - subtitle_text.get_width() // 2,
+                                        subtitle_text.get_height() * 2 + padding))
+
+        update()
+        button = get_input()
+        if button == 1:
+            pygame.draw.rect(right_surface, color_black, right_surface.get_rect(), 2)
+            right_surface.blit(right_text, (right_surface.get_width() // 2 - right_text.get_width() // 2,
+                                            right_surface.get_height() // 2 - right_text.get_height() // 2))
+            screen.blit(right_surface, (WIDTH // 2 - right_surface.get_width() // 2, HEIGHT // 2))
+
+            update()
+            pygame.time.delay(100)
+            return 1
+        if button == 2:
+            pygame.draw.rect(left_surface, color_black, left_surface.get_rect(), 2)
+            left_surface.blit(left_text, (left_surface.get_width() // 2 - left_text.get_width() // 2,
+                                          left_surface.get_height() // 2 - left_text.get_height() // 2))
+            screen.blit(left_surface, (WIDTH // 2 - left_surface.get_width() // 2, HEIGHT * 3 // 4))
+
+            update()
+            pygame.time.delay(100)
+            return 2
 
 
 # INITIALIZATION
 
 # Start display
 
-OLED.Device_Init()
+if DISPLAYTYPE == "OLED":
+    OLED.Device_Init()
+elif DISPLAYTYPE == "HDMI":
+    os.environ['SDL_VIDEO_CENTERED'] = '1'
+    pygame.init()
+    size = WIDTH, HEIGHT
+    pygame.display.set_caption("KalEYEdoscope")
+    screen = pygame.display.set_mode(size, pygame.NOFRAME)
+    clock = pygame.time.Clock()
 
 # Initialize buttons
 
-GPIO.setup(BUTTON1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(BUTTON2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+if INTYPE == "BUTTON":
+    GPIO.setup(BUTTON1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(BUTTON2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 # Load frequently used variables
 
@@ -140,12 +230,25 @@ data = yaml.safe_load(open("data/data"))
 state = "Start"
 
 # Fonts
-font_title = ImageFont.truetype('cambriab.ttf', 24)
-font_subtitle = ImageFont.truetype('cambriab.ttf', 16)
-font_normal = ImageFont.truetype('cambriab.ttf', 18)
+if DISPLAYTYPE == "OLED":
+    font_title = ImageFont.truetype('cambriab.ttf', 24)
+    font_subtitle = ImageFont.truetype('cambriab.ttf', 16)
+    font_normal = ImageFont.truetype('cambriab.ttf', 18)
+else:
+    font_title = pygame.font.Font(None, 66)
+    font_subtitle = pygame.font.Font(None, 48)
+    font_normal = pygame.font.Font(None, 66)
 
 # Spacings
 padding = 2
+
+# Colors
+color_black = (0, 0, 0)
+color_gray = (100, 100, 100)
+color_white = (255, 255, 255)
+color_background = (105, 105, 105)
+color_right = (0, 0, 255)
+color_left = (255, 140, 0)
 
 
 # ACTIVITY FUNCTIONS
@@ -155,15 +258,33 @@ def start():
     Main screen event.
     """
     global state
-    draw, image = clear_screen()
-    welcome = "Welcome to your"
-    title = "KalEYEdoscope"
-    welcome_text = draw.textsize(welcome, font_subtitle)
-    title_text = draw.textsize(title, font_subtitle)
-    draw.text((WIDTH // 2 - welcome_text[0] // 2, welcome_text[1]), welcome, font=font_subtitle, fill="BLACK")
-    draw.text((WIDTH // 2 - title_text[0] // 2, welcome_text[1] * 2 + padding), title, font=font_subtitle, fill="BLACK")
+    if DISPLAYTYPE == "OLED":
+        draw, image = clear_screen()
+        welcome = "Welcome to your"
+        title = "KalEYEdoscope"
+        welcome_text = draw.textsize(welcome, font_subtitle)
+        title_text = draw.textsize(title, font_subtitle)
+        draw.text((WIDTH // 2 - welcome_text[0] // 2, welcome_text[1]), welcome, font=font_subtitle, fill="BLACK")
+        draw.text((WIDTH // 2 - title_text[0] // 2, welcome_text[1] * 2 + padding), title, font=font_subtitle,
+                  fill="BLACK")
 
-    button = update_buttons(draw, image, "Exit", "Start")
+        button = update_buttons("Exit", "Start", update_draw=draw, update_image=image)
+
+    else:
+        clear_screen()
+        text = font_title.render("Welcome to your", True, color_white)
+        surface = pygame.Surface(font_title.size("Welcome to your"))
+        surface.fill(color_background)
+        surface.blit(text, (0, 0))
+        text2 = font_title.render("KalEYEdoscope", True, color_white)
+        surface2 = pygame.Surface(font_title.size("KalEYEdoscope"))
+        surface2.fill(color_background)
+        surface2.blit(text2, (0, 0))
+        screen.blit(surface, (WIDTH // 2 - text.get_width() // 2, text.get_height()))
+        screen.blit(surface2, (WIDTH // 2 - text2.get_width() // 2, text.get_height() * 2 + padding))
+        update()
+        button = update_buttons("Exit", "Start")
+        update()
 
     if button == 1:
         if data["new_user"]:
@@ -187,7 +308,7 @@ def baseline():
     data["number_of_tests"] = -1
     update_data()
 
-    update_buttons(draw, image, "Start", "Start", "Record a ", "baseline.")
+    update_buttons("Start", "Start", "Record a ", "baseline.", draw, image)
     state = "Test"
 
 
@@ -219,18 +340,29 @@ def display_circle(angle, theta, r):
                       facecolor='k')
     plt.close(plt.gcf())
 
-    circle_image = Image.open("images/current_circle.png", mode='r')
-    draw = ImageDraw.Draw(circle_image)
+    if DISPLAYTYPE == "OLED":
+        circle_image = Image.open("images/current_circle.png", mode='r')
+        draw = ImageDraw.Draw(circle_image)
+        # Center cross hair
+        draw.line((WIDTH // 2, HEIGHT // 2 - 2, WIDTH // 2, HEIGHT // 2 + 2))
+        draw.line((WIDTH // 2 - 2, HEIGHT // 2, WIDTH // 2 + 2, HEIGHT // 2))
 
-    # Center cross hair
-    draw.line((WIDTH // 2, HEIGHT // 2 - 2, WIDTH // 2, HEIGHT // 2 + 2))
-    draw.line((WIDTH // 2 - 2, HEIGHT // 2, WIDTH // 2 + 2, HEIGHT // 2))
+        OLED.Clear_Screen()
+        update(circle_image)
+        OLED.Delay(500)
+    elif DISPLAYTYPE == "HDMI":
+        circle = pygame.image.load("images/current_circle.png")
+        clear_screen()
+        screen.blit(circle, (WIDTH // 2 - circle.get_width() // 2,
+                             HEIGHT // 2 - circle.get_height() // 2))
+        pygame.draw.line(screen, color_white, (WIDTH // 2, HEIGHT // 2 - 5), (WIDTH // 2, HEIGHT // 2 + 5))
+        pygame.draw.line(screen, color_white, (WIDTH // 2 - 5, HEIGHT // 2), (WIDTH // 2 + 5, HEIGHT // 2))
+        update()
+        pygame.time.delay(500)
+        clear_screen()
 
-    OLED.Clear_Screen()
-    update(circle_image)
-    OLED.Delay(500)
     draw, image = clear_screen()
-    button = update_buttons(draw, image, "No", "Yes", "Did the circle", "appear normal?")
+    button = update_buttons("No", "Yes", "Did the circle", "appear normal?", draw, image)
 
     # Note that choice returns "y" for a distorted circle and "n" for a normal circle
     if button == 1:
@@ -251,7 +383,7 @@ def test():
 
     # Pick eye
     draw, image = clear_screen()
-    button = update_buttons(draw, image, "Left", "Right", "Select an eye", "to test.")
+    button = update_buttons("Left", "Right", "Select an eye", "to test.", draw, image)
     eye = "Error"
 
     if button == 1:
@@ -329,87 +461,17 @@ def test():
     update_data()
 
     draw, image = clear_screen()
-    update_buttons(draw, image, "Start Menu", "Start Menu", "Test Completed")
+    update_buttons("Start Menu", "Start Menu", "Test Completed", draw, image)
     state = "Start"
 
 
 def program_quit():
-    OLED.Clear_Screen()
-    GPIO.cleanup()
-    exit()
-
-
-# def profile():
-#     """
-#     View profile event
-#     """
-#     scores = data["scores"]
-#     plt.plot(scores)
-#     plt.ylabel("Scores")
-#     plt.xlabel("Test")
-#     plt.gcf().savefig('images/plot.png', bbox_inches='tight', dpi=(WIDTH // 5))
-#     plt.close(plt.gcf())
-#
-#     average = sum(scores) / len(scores)
-#
-#     text = font_normal.render("Average score: %.2f" % average, True, color_black)
-#
-#     plot = pygame.image.load("images/plot.png")
-#     clear_screen()
-#
-#     game.screen.blit(plot, (game.w // 2 - plot.get_width() // 2, 0))
-#     game.screen.blit(text, (game.w // 2 - text.get_width() // 2, plot.get_height() + padding))
-#     update_buttons("Start Menu", "Advanced")
-#     update()
-#
-#     running = True
-#     while running:
-#         for e in pygame.event.get():
-#             if e.type == pygame.QUIT:
-#                 sys.exit()
-#             if e.type == pygame.KEYDOWN:
-#                 if e.key == pygame.K_r:
-#                     game.state = "Advanced"
-#                     running = False
-#                 elif e.key == pygame.K_l:
-#                     game.state = "Start"
-#                     running = False
-
-
-# def advanced():
-#     df = pd.DataFrame({"Passed": data["passed"],
-#                        "Eye": data["eye"]})
-#
-#     model = sm.formula.glm("Passed ~ Eye",
-#                            family=sm.families.Binomial(),
-#                            data=df).fit()
-#     print(model.summary())
-#     clear_result_count = 5
-#     clear_screen()
-#     update_buttons("Start Menu", "Clear Results (%d)" % clear_result_count)
-#     update()
-#     running = True
-#     while running:
-#         for e in pygame.event.get():
-#             if e.type == pygame.QUIT:
-#                 sys.exit()
-#             if e.type == pygame.KEYDOWN:
-#                 if e.key == pygame.K_l:
-#                     game.state = "Start"
-#                     running = False
-#                 elif e.key == pygame.K_r:
-#                     if clear_result_count == 0:
-#                         game.state = "Start"
-#                         data.pop("user")
-#                         data.pop("scores")
-#                         data.pop("eye")
-#                         update_data()
-#                         running = False
-#                     else:
-#                         clear_result_count -= 1
-#                         clear_screen()
-#                         update_buttons("Start Menu", "Clear Results (%d)" % clear_result_count)
-#                         update()
+    if DISPLAYTYPE == "OLED":
+        OLED.Clear_Screen()
+        GPIO.cleanup()
+        exit()
+    else:
+        sys.exit()
 
 
 # MAIN RUNNING LOOP
@@ -422,12 +484,6 @@ while True:
 
     elif state == "Test":
         test()
-
-    # if state == "Profile":
-    #     profile()
-    #
-    # if state == "Advanced":
-    #     advanced()
 
     elif state == "Quit":
         program_quit()
